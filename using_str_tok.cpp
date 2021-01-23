66f0ea9b20c0543b3546269c28eb0e7131eb36df
Murtaza-Kazmi <murtaza.kazmi.75@gmail.com>
2021-01-23 14:32:19 -0745

    str tok

---
+++ using_str_tok.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+/*
+Imports specs
+	stdio for printf
+	unistd for read
+	string for strtok
+	iostream for stoi
+*/
+
+//for cout:
+using namespace std;
+
+int main() {
+	
+	char inputs[50];
+	
+	int count = read(0, inputs, 50);
+	
+	if (errno == EBADF){
+		cout << "Error in read\n";
+		exit(1);
+	}
+	
+	for(int  i= 0; i < count-1; i++){
+		if(inputs[i] != ' '){
+		if(!isdigit(inputs[i])){
+			cout << "Invalid argument error\n";
+			exit(1);
+		}
+		}
+		
+	}
+	
+	char* token = strtok(inputs, " ");
+
+	int total = 0;
+	
+	while(token != NULL) {
+		
+		total += stoi(token);
+		token = strtok(NULL, " "); 
+		
+	}
+	
+	printf("%d \n", total);
+	
+	return 0;
+}
