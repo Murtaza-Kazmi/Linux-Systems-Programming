5d790c4d879c1105ed8b4f18c824eb49a20657e7
Murtaza-Kazmi <murtaza.kazmi.75@gmail.com>
2021-03-18 10:39:46 +0500

    kill error handling

--- kill.cpp
+++ kill.cpp
@@ -10,10 +10,14 @@ int main(int argc, char* argv[]){
 	
 	cout << argc << '\n';
 	
+	if(argc != 3)	cout << "Invalid number of arguments\n";
+		
+	
 	for(int i = 0; i < argc; i++) cout << argv[i] << '\n';
 	
 	cout << "Done printing args\n\n";
 	string h = argv[1];
+
 	if(h == "kill"){
 
 	int pid = atoi(argv[2]);
