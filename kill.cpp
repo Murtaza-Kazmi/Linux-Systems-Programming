df9a6495154248ee9906bc218c64450a35f9a480
Murtaza-Kazmi <murtaza.kazmi.75@gmail.com>
2021-03-18 10:24:43 +0500

    kill

---
+++ kill.cpp
@@ -0,0 +1,31 @@
+#include <sys/types.h>
+#include <signal.h>
+
+#include <iostream>
+
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[]){
+	
+	cout << argc << '\n';
+	
+	for(int i = 0; i < argc; i++) cout << argv[i] << '\n';
+	
+	cout << "Done printing args\n\n";
+	string h = argv[1];
+	if(h == "kill"){
+
+	int pid = atoi(argv[2]);
+	
+	cout << pid << '\n';
+	
+	kill(pid, SIGTERM);
+	}
+	else{
+		cout << "Error" <<'\n';
+	}
+
+
+	return 0;
+}
