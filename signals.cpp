32e625cee1e8970faa6d30e5434408cdd3d6c4c2
Murtaza-Kazmi <murtaza.kazmi.75@gmail.com>
2021-04-14 00:45:56 +0500

    asynch signals accomplish

--- signals.cpp
+++ signals.cpp
@@ -11,14 +11,17 @@
 #include <signal.h>
 
 /* handler for SIGINT */
- static void sigint_handler (int signo){
+  static void sigint_handler (int signo){
     char buff[20];
     sprintf (buff, "Caught SIGINT!\n");
     puts(buff);
-    // exit(EXIT_SUCCESS);
     }
+const union sigval x {
+    // int   sival_int = 0;
+    // void *sival_ptr;
+};
 
-int main (void){
+int main(void){
 
     int size = 100;
     char buff1[size];
@@ -31,15 +34,10 @@ int main (void){
         buff0[i] = '\0';
     }
 
-    // struct sigaction sa;
-    // sa.sa_handler = &sigint_handler;
-    //sigaction(SIGINT, &sa, NULL) == -1
-    if (signal (SIGINT, sigint_handler) == SIG_ERR) {
-            char buff1[30];
-            sprintf (buff1, "Cannot handle SIGINT!\n");
-            puts(buff1);
-            exit(EXIT_FAILURE);
-        }
+    struct sigaction sa;
+    sa.sa_handler = &sigint_handler;
+    
+    
 
     int pid = fork();
     
@@ -48,52 +46,60 @@ int main (void){
         puts(buff1);
     }
     else if(pid == 0){//server
-
+        // if (signal(SIGINT, sigint_handler) == SIG_ERR) {
+        if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+                char buff1[30];
+                sprintf (buff1, "Cannot handle SIGINT!\n");
+                puts(buff1);
+                exit(EXIT_FAILURE);
+            }
+        // sleep(5000);
         pause();
+        
         sprintf (buff1, "Server: signal received\n");
         puts(buff1);
 
         //opens file
-        int f = open("z.txt", O_RDONLY | O_CREAT | O_EXCL );
+        int f = open("z.txt", O_RDONLY | O_CREAT );
         if(f == -1){
             perror("In opening file in server excl.");
-            exit(0);
+            exit(EXIT_FAILURE);
         }
         
         //reads from file
         int input = read(f, buff0, 2);
         if(input == -1){
             perror("Server: In taking input");
-            exit(0);
+            exit(EXIT_SUCCESS);
         }
 
         //calculates sum
         int res = 0;
         for(int i = 0; i < strlen(buff0); i++){
-            res += (int) buff0[i];
+            res += (int) buff0[i] - '0';
         }
 
         sprintf(buff1, "%d", res);
         
         //opens file
-        f = open("z.txt", O_WRONLY | O_CREAT | O_APPEND );
+        f = open("z.txt", O_WRONLY | O_TRUNC | O_APPEND );
         if(f == -1){
             perror("In opening file in server excl.");
-            exit(0);
+            exit(EXIT_FAILURE);
         }
 
         //writes sum to file
         if(write(f,buff1, 2) == -1){
             perror("In writing to file in client");
-            exit(0);
+            exit(EXIT_SUCCESS);
         }
 
         //sends signal to child
-        // sigqueue(getppid(), , const union sigval value);
+        // int sigRes = sigqueue(getppid(), SIGUSR1, x);
         int sigRes = kill(getppid(), SIGUSR1);
         if(sigRes == -1){
             perror("In error handling");
-            exit(0);
+            exit(EXIT_FAILURE);
         }
         sprintf (buff1, "Server: signal sent\n");
         puts(buff1);
@@ -101,31 +107,40 @@ int main (void){
     }
     else{//client
 
+        // if (signal(SIGINT, sigint_handler) == SIG_ERR) {
+        if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+                char buff1[30];
+                sprintf (buff1, "Cannot handle SIGINT!\n");
+                puts(buff1);
+                exit(EXIT_FAILURE);
+            }
+
         //takes input
         int input = read(0, buff0, 2);
         if(input == -1){
             perror("In taking input");
-            exit(0);
+            exit(EXIT_SUCCESS);
         }
 
         //writes to file
         int f = open("z.txt", O_WRONLY | O_CREAT, S_IRWXU);
         if(f == -1){
             perror("In opening file in client");
-            exit(0);
+            exit(EXIT_FAILURE);
         }
 
         int res = write(f,buff0, 2);
         if(res == -1){
             perror("In writing to file in client");
-            exit(0);
+            exit(EXIT_FAILURE);
         }
 
         //sends signal to child
+        // int sigRes = sigqueue(pid, SIGUSR1, x);
         int sigRes = kill(pid, SIGUSR1);
         if(sigRes == -1){
             perror("In error handling");
-            exit(0);
+            exit(EXIT_FAILURE);
         }
         sprintf (buff1, "Client: signal sent\n");
         puts(buff1);
@@ -139,13 +154,13 @@ int main (void){
         f = open("z.txt", O_RDONLY | O_CREAT);
         if(f == -1){
             perror("In opening file in client");
-            exit(0);
+            exit(EXIT_FAILURE);
         }
 
         int output = read(f, buff0, 10);
         if(output == -1){
             perror("In reading program output");
-            exit(0);
+            exit(EXIT_FAILURE);
         }
 
         //prints result
